3217d78c90ed6d3d17cbaabc3e6a344caff60a16
Andreina Di Brigida <Akiarakirax3@gmail.com>
2026-09-03 20:01:23 -0400

    Update og image name

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -11,7 +11,7 @@ export const SITE_DESCRIPTION =
 export const SITE_URL =
   process.env.NEXT_PUBLIC_SITE_URL ?? "https://easy-vcard-manager.vercel.app";
 
-export const SITE_OG_IMAGE = "/image-og%20(1).png";
+export const SITE_OG_IMAGE = "/easy-vcard-manager-og.png";
 
 export const APP_URL = "https://easy-vcard-manager-app.vercel.app/";
 
